832f2d585842842f46fa170bf49d80af6604b176
sebastiangomezzz <sebastiangomezzz@gmail.com>
2021-06-02 16:34:25 +0200

    erased 1 file

--- 00_exercise-js_arrays_basic_methods.js
+++ 00_exercise-js_arrays_basic_methods.js
@@ -1,6 +1,19 @@
 /**
  * Solution of basic methods exercise
  */
+//Prime Numbers
+ function checkPrime(number) {
+    
+    if (number === 0 || number === 1) return false
+    
+    for (let i = 2; i < number; i++) { //contamos a partir de dos
+      if (number % i === 0) return false
+    }
+    return true;
+}
+
+//------------------------------------------------
+//Digit sum
  function sumDigits(numberToSum) {
     let totalSum = 0
 
@@ -15,10 +28,42 @@
 console.log(sumDigits(324564))
 
 
-
+//------------------------------------------------
+//Pythagoras
 function calculateSide(sideA, sideB) {
     let sideC = sideA.SQRTsideA + sideB.SQRTsideB
+
     return sideC
 }
 
 console.log(calculateSide(8, 6))
+//returns NaN?
+
+//-----------------------------------------------
+//Second Numbers
+
+const numbersArray = [4,2,6,9,5]
+
+const numbersArraySorted = numbersArray.slice().sort();
+console.log(numbersArraySorted);
+
+// const numbersArray = [4,2,6,9,5]
+// function secondNumbers(numbersArray) {
+//     let secondHighNum = 0;
+//     let secondLowNum = 0;
+//     let numberToCompare 
+    
+//     numbersArray.forEach(numberToCompare => {
+//         if (secondHighNum < numberTocompare) {
+//             secondHighNum = numberToCompare
+//             return secondHighNum
+//         } 
+//         if(secondLowNum > numberToCompare){
+//             secondLowNum = numbertocompare
+//             return secondLowNum
+//         }
+//     });
+// }
+// console.log(secondNumbers(numbersArray));
+//=> second lowest: 4
+//=> second highest: 6
--- 00_prime_numbers.js
+++
@@ -1,9 +0,0 @@
-function checkPrime(number) {
-    
-    if (number === 0 || number === 1) return false
-    
-    for (let i = 2; i < number; i++) { //contamos a partir de dos
-      if (number % i === 0) return false
-    }
-    return true;
-}
