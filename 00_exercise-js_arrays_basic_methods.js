1f7f18f8785e3c0e5d821db41a7d967d6dfcd0f5
sebastiangomezzz <sebastiangomezzz@gmail.com>
2021-06-02 14:26:27 +0200

    1rst and 2nd iterations

--- 00_exercise-js_arrays_basic_methods.js
+++ 00_exercise-js_arrays_basic_methods.js
@@ -1,3 +1,24 @@
 /**
  * Solution of basic methods exercise
  */
+ function sumDigits(numberToSum) {
+    let totalSum = 0
+
+    const splitedNumber = numberToSum.toString().split("")
+
+    splitedNumber.forEach(eachNumber => {
+        totalSum += parseInt(eachNumber)
+    });
+    return totalSum
+}
+
+console.log(sumDigits(324564))
+
+
+
+function calculateSide(sideA, sideB) {
+    let sideC = sideA.SQRTsideA + sideB.SQRTsideB
+    return sideC
+}
+
+console.log(calculateSide(8, 6))
